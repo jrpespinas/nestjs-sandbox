1ea74d2ff6215d262c39e6712891f70ee29385a1
jrpespinas <jrpespinas@gmail.com>
2023-02-06 00:01:59 +0800

    src: Implement Test Environment e2e Init Testing

--- nest-crud/src/main.ts
+++ nest-crud/src/main.ts
@@ -6,8 +6,6 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
-declare const module: any;
-
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
@@ -18,11 +16,6 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
   await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
--- nest-crud/test/app.e2e-spec.ts
+++ nest-crud/test/app.e2e-spec.ts
@@ -1,11 +1,32 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 
 describe('App e2e', () => {
+  let app: INestApplication;
+
+  // Compile the app before running the test
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+
+    // Simulate the app
+    // (see: src/main.ts)
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+
+    await app.init();
   });
+
+  // Close app after testing everything
+  afterAll(() => {
+    app.close();
+  });
+
   it.todo('should pass');
 });
